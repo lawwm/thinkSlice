0e2e824068e61ed1413ccb185ae8703365e837b3
weiming <weiming.law@gmail.com>
2021-05-28 20:58:29 +0800

    404 error fix v3

--- appmanager/appmanager/settings.py
+++ appmanager/appmanager/settings.py
@@ -118,8 +118,9 @@ DATABASES = {
     }
 }
 
-
-DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
+# DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 # print([DATABASES['default']])
 
 # Password validation
