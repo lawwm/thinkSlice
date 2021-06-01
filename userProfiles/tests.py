53cc8a915b6e75b71f326694be698b23ef749938
lowkangn <e0543462@u.nus.edu>
2021-06-01 20:36:38 +0800

    profile backend tweaks to allow more edits

--- userProfiles/tests.py
+++ userProfiles/tests.py
@@ -84,6 +84,7 @@ class GetPatchPutDeleteProfileAPI(TestCase):
     # Patch profile details by id
     def test_edit_profile_details_id(self):
         editData = {
+            "tutor_contact": "12345678",
             "tutor_whatsapp": 12345678,
             "tutor_telegram": "@honestman",
             "aggregate_star": "4.5",
@@ -104,14 +105,16 @@ class GetPatchPutDeleteProfileAPI(TestCase):
             content_type='application/json' ,**self.headers)
         parsed = json.loads(response.content)
         self.assertEqual(response.status_code, 200)
+        self.assertEqual(parsed["tutor_contact"], "12345678")
         self.assertEqual(parsed["tutor_whatsapp"], 12345678)
         self.assertEqual(parsed["tutor_telegram"], "@honestman")
         self.assertEqual(parsed["aggregate_star"], 4.5)
 
         # PUT profile clear the profile details while not being the owner
         clearData = {
+                "tutor_contact": "",
                 "tutor_whatsapp": None,
-                "tutor_telegram"
+                "tutor_telegram": None,
                 "aggregate_star": None,
                 "duration_classes": None,
                 "subjects": None,
