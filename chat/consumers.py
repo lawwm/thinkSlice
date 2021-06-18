fe56fb301376d8c9f1bab7970e623092c74a2a3e
lowkangn <e0543462@u.nus.edu>
2021-06-18 18:53:39 +0800

    basic chat implemented

--- chat/consumers.py
+++ chat/consumers.py
@@ -19,19 +19,18 @@ class ChatConsumer(WebsocketConsumer):
         self.room_group_name = 'chat_%s' % self.room_name
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
-        ))
-
+        )
         self.accept()
 
     def disconnect(self, close_code):
         # Leave room group
-        async_to_sync(self.channel_layer.group_discard(
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
-        ))
+        )
 
     def message_to_json(self, message):
         return {
@@ -59,13 +58,13 @@ class ChatConsumer(WebsocketConsumer):
         self.send_message(content)
 
     def send_chat_message(self, message):
-        async_to_sync(self.channel_layer.group_send(
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message
             }
-        ))
+        )
 
     def new_message(self, data):
         author = get_object_or_404(User, id=data['from'])
@@ -94,7 +93,4 @@ class ChatConsumer(WebsocketConsumer):
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
-        print(message)
-        self.send(text_data=json.dumps({
-            'message': message
-        }))
+        self.send(text_data=json.dumps(message))
