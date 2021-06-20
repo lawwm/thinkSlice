1788ba3d42065790d68bec2198185c81f711474f
weiming <weiming.law@gmail.com>
2021-06-20 18:01:58 +0800

    Fix v73 cors error

--- appmanager/asgi.py
+++ appmanager/asgi.py
@@ -2,11 +2,11 @@ import os
 
 
 from django.core.asgi import get_asgi_application
-import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appmanager.settings")
 django_asgi_app = get_asgi_application()
 
+import chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
