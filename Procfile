4e0efc87f7a1f12ce8be8fa15dfd7f93db736118
weiming <weiming.law@gmail.com>
2021-06-20 16:56:01 +0800

    Fix attempt #3 cors

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: daphne appmanager.asgi:channel_layer --port $PORT --bind 0.0.0.0 -v2
-chatworker: python manage.py runworker --settings=chat.settings -v2
+chatworker: python manage.py runworker --settings=appmanager.settings -v2
--- appmanager/asgi.py
+++ appmanager/asgi.py
@@ -1,4 +1,5 @@
 import os
+import channels.asgi
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -6,6 +7,8 @@ from django.core.asgi import get_asgi_application
 import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appmanager.settings")
+
+channel_layer = channels.asgi.get_channel_layer()
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
