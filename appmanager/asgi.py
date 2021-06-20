d11b75546c910570b01b774d9e34c37c1c89d78b
weiming <weiming.law@gmail.com>
2021-06-20 17:07:52 +0800

    Cors fix attempt #4

--- appmanager/asgi.py
+++ appmanager/asgi.py
@@ -1,5 +1,4 @@
 import os
-import channels.asgi
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -7,8 +6,6 @@ from django.core.asgi import get_asgi_application
 import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appmanager.settings")
-
-channel_layer = channels.asgi.get_channel_layer()
 
 application = ProtocolTypeRouter({
   "http": get_asgi_application(),
