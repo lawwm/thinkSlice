8bcb11f3259d1868d42c3fcbb40c0b661adafea4
lowkangn <e0543462@u.nus.edu>
2021-06-21 15:51:44 +0800

    deployment fixes

--- appmanager/settings.py
+++ appmanager/settings.py
@@ -117,7 +117,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [(os.environ.get('REDIS_URL'), 'redis://localhost:6379')],
+            "hosts": [os.environ.get('REDIS_URL')],
         },
     },
 }
