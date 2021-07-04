68c7b9cf38a79f77681f2339d3785b9863501c01
lowkangn <e0543462@u.nus.edu>
2021-07-04 13:32:27 +0800

    backend changes for chat

--- appmanager/asgi.py
+++ appmanager/asgi.py
@@ -1,21 +1,14 @@
 import os
 
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appmanager.settings")
-django_asgi_app = get_asgi_application()
-
-import chat.routing
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+import chat.routing
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appmanager.settings")
 
 application = ProtocolTypeRouter({
-    # Django's ASGI application to handle traditional HTTP requests
-  "http": django_asgi_app,
-
-    # WebSocket chat handler
+  "http": get_asgi_application(),
   "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
